389b2394d8e053f47af5042d4fde264a24cb5fcb
Robert Gleim <rgleim@outlook.com>
2025-10-23 13:49:16 -0500

    cors fix

--- dev_app.py
+++ dev_app.py
@@ -1,7 +1,9 @@
 from app import create_app
 from app.models import db
+from flask_cors import CORS
 
 app = create_app('DevelopmentConfig')
+CORS(app)
 
 with app.app_context():
     # db.drop_all() for tersting purposes
