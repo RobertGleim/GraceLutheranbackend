b5748c5374bafb17f0fa307f9c55910c3a328cf1
Robert Gleim <rgleim@outlook.com>
2025-10-31 13:02:24 -0500

    swagger

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,11 @@
 from app import create_app
 from app.models import db
 from flask_cors import CORS
+from flasgger import Swagger
 
 app = create_app('ProductionConfig')
 CORS(app)
+Swagger(app)  # Add this line to enable Swagger UI
 
 with app.app_context():
     # db.drop_all() for tersting purposes
